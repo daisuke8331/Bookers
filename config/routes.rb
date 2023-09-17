b7710c828d4702911702fb75cb054154463f8d06
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-09-17 13:26:45 +0000

    bookers-start2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resource :books
+  resources :books
   get '/top' => 'homes#top'
+  get 'lists/:id' => 'lists#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
