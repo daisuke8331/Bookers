e37b2a0d4f10a82605815eabcc314c027cc72b6c
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-09-18 08:01:17 +0000

    bookers-finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,14 @@ class BooksController < ApplicationController
   def create
 
     @booknew = Book.new(book_params)
-    @booknew.save
+    if @booknew.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@booknew.id)
+    else
+      @books = Book.all
+      render :index
 
-    redirect_to book_path(@booknew.id)
+    end
   end
 
   def show
@@ -21,17 +26,24 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      #@book = Book.find(params[:id]) エラーメッセージを上書きしてしまっていたので削除
+      render :edit
+    end
   end
 
   def destroy
-    book = Book.find(params:id)
+    book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was succesfully destroyed"
     redirect_to '/books'
   end
 
